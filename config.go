6755f5bacf4c68c4271252f30ec06cc7ff106642
vmax <chn.vmax@gmail.com>
2025-06-26 17:22:13 +0800

    Bug Fix: YML configuration file and ENV confusion issue.

--- config.go
+++ config.go
@@ -13,18 +13,19 @@ type Config struct {
 	// APISIX 管理 API 地址和密钥
 	// 支持通过环境变量 APISIX_ADMIN_API 和 APISIX_ADMIN_KEY 设置
 	// 如果未设置，则使用默认值 http://
-	AdminAPI      string        `yaml:"admin_api"`
-	AdminKey      string        `yaml:"admin_key"`
-	ServiceName   string        `yaml:"service_name"`
-	ServiceID     string        `yaml:"service_id"`
-	ServicePort   int           `yaml:"service_port"`
-	ProtoPath     string        `yaml:"proto_path"`
-	ProtoPbPath   string        `yaml:"proto_pb_path"`
-	RoutePlugins  []PluginSpec  `yaml:"route_plugins"`
-	Upstream      *UpstreamSpec `yaml:"upstream,omitempty"`
-	TTL           int           `yaml:"ttl"`
-	MaxRetry      int           `yaml:"max_retry"`
-	RetryInterval time.Duration `yaml:"retry_interval"`
+	AdminAPI       string        `yaml:"admin_api"`
+	AdminKey       string        `yaml:"admin_key"`
+	ServiceVersion string        `yaml:"service_version"`
+	ServiceName    string        `yaml:"service_name"`
+	ServiceID      string        `yaml:"service_id"`
+	ServicePort    int           `yaml:"service_port"`
+	ProtoPath      string        `yaml:"proto_path"`
+	ProtoPbPath    string        `yaml:"proto_pb_path"`
+	RoutePlugins   []PluginSpec  `yaml:"route_plugins"`
+	Upstream       *UpstreamSpec `yaml:"upstream,omitempty"`
+	TTL            int           `yaml:"ttl"`
+	MaxRetry       int           `yaml:"max_retry"`
+	RetryInterval  time.Duration `yaml:"retry_interval"`
 }
 
 type PluginSpec struct {
@@ -51,6 +52,12 @@ func LoadConfig(path string) (*Config, error) {
 		}
 	}
 	// ENV 覆盖
+	if v := os.Getenv("APISIX_ADMIN_API"); v != "" {
+		cfg.AdminAPI = v
+	}
+	if v := os.Getenv("APISIX_ADMIN_KEY"); v != "" {
+		cfg.AdminKey = v
+	}
 	if v := os.Getenv("SERVICE_NAME"); v != "" {
 		cfg.ServiceName = v
 	}
@@ -66,14 +73,29 @@ func LoadConfig(path string) (*Config, error) {
 	if v := os.Getenv("PROTO_PB_PATH"); v != "" {
 		cfg.ProtoPbPath = v
 	}
-	if v := os.Getenv("MAX_RETRY"); v != "" {
+	if v := os.Getenv("REGISTRY_TTL"); v != "" {
+		if n, err := strconv.Atoi(v); err == nil {
+			cfg.TTL = n
+		}
+	}
+	if v := os.Getenv("REGISTRY_MAX_RETRY"); v != "" {
 		if n, err := strconv.Atoi(v); err == nil {
 			cfg.MaxRetry = n
 		}
 	}
-
+	if v := os.Getenv("REGISTRY_RETRY_INTERVAL"); v != "" {
+		if duration, err := time.ParseDuration(v); err == nil {
+			cfg.RetryInterval = duration
+		}
+	}
+	if cfg.TTL < 60 {
+		cfg.TTL = 60 // 默认值
+	}
 	if cfg.MaxRetry <= 0 {
-		cfg.MaxRetry = 3 // 或你想要的默认重试次数
+		cfg.MaxRetry = 3 // 默认值
+	}
+	if cfg.RetryInterval < 3*time.Second {
+		cfg.RetryInterval = 3 * time.Second
 	}
 	return cfg, nil
 }
